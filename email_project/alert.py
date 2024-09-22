e8a0b26f4a9301f9ad94bc47ad868e1146dc7d2f
mazuckerberg-git <144284486+mazuckerberg-git@users.noreply.github.com>
2024-09-22 17:30:43 -0400

    added DOCSTRING

--- email_project/alert.py
+++ email_project/alert.py
@@ -3,14 +3,28 @@ from mailer import send_email
 
 
 def setup_args(parser):
-    '''So if I type this'''
+    """
+    Setup the command line arguments for the email that will be sent.
+
+    Arguments:
+        parser - ArgumentParser object.
+
+    Returns:
+        ArgumentParser object with the required arguments added.
+    """
+
     parser.add_argument("-s", "--sender", type=str, required=True)
     parser.add_argument("-r", "--recipient", type=str, required=True)
     parser.add_argument("-j", "--subject", type=str, default="Subject")
     parser.add_argument("-b", "--body", type=str, default="Body")
+
     return parser
 
 def main():
+    """
+    Main function to send an email using the function  setup_args and send_email created in mailer.py.
+    """
+
     parser = ArgumentParser()
     args = setup_args(parser).parse_args()
 
